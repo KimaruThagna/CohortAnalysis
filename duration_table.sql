5892ce074b829fa06b96260f870d0ad5b0e90b59
Kimaru Thagana <Kimaru.Thagana@M-KOPA.local>
2022-08-30 08:46:48 +0300

    add duration table

--- duration_table.sql
+++ duration_table.sql
@@ -1,16 +1,24 @@
-
 -- To build a duration table, we define a censoring date(cut off point of the analysis)
 -- can be arbitrary date or maximum date in your dataset.
 -- We then calulate duration from start date to EITHER end date where target action happens or censoring date in case action does not happen
+{% set censoring_time_query %}
+SELECT MAX(EventDate) AS CensorDate FROM {{ref('events')}}
+{% endset %}
+
+{% set censoring_time = run_query(censoring_time_query) %}
+{% if execute %}
+-- Return the date
+{% set max_date = censoring_time.columns[0].values()[0] %}
+{% else %}
+{% set max_date = [] %}
+{% endif %}
 
 WITH user_start_dates AS
 (
     SELECT UserId, RegistrationDate
     FROM {{ref('users')}}
 )
-, censoring_time AS (
-    SELECT MAX(EventDate) FROM {{ref('events')}}
-)
+
 -- find out the first SYSTEM ERROR event per user. Remember our goal is assessing how long until FIRST error occurs
 , first_occurence_of_target_event AS (
  SELECT 
@@ -18,7 +26,7 @@ WITH user_start_dates AS
     FROM (
         SELECT
             *,
-            ROW_NUMBER() OVER(PARTITION BY visitorid ORDER BY event_at ASC) AS row_num
+            ROW_NUMBER() OVER(PARTITION BY UserId ORDER BY EventDate ASC) AS row_num
         FROM {{ref('events')}} WHERE EventType = 'SYSTEM ERROR'
     ) AS _
     WHERE row_num = 1
@@ -26,9 +34,9 @@ WITH user_start_dates AS
 
 , final AS (
 SELECT 
-users.UserId, users.RegistrationDate, target_events.EventType, 
-COALESCE(target_events.EventDate, censoring.EventDate) - users.RegistrationDate as duration
-FROM user_start_dates as users, censoring LEFT JOIN first_occurence_of_target_event as target_events
+users.UserId, users.RegistrationDate, target_events.EventType,
+DATEDIFF(DAY,  users.RegistrationDate , COALESCE(target_events.EventDate, CAST('{{max_date}}' AS DATE) ) ) AS days_to_event
+FROM user_start_dates as users LEFT JOIN first_occurence_of_target_event as target_events
 ON users.UserId = target_events.UserId
 )
 SELECT * FROM final
